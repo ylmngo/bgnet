fe9581d4cf662c397d47e188887cee08176641a1
ylmngo <aamerasim45@gmail.com>
2023-03-19 21:53:10 +0530

    Second Commit

--- server.c
+++ server.c
@@ -10,41 +10,34 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
 
-int main(int argc, char* argv[]) {
-    int status; 
-    char ipstr[INET6_ADDRSTRLEN]; 
-    struct addrinfo hints, *res; 
+#define MYPORT "3490"
+#define BACKLOG 10  
 
+int main(int argc, char* argv[]) {
+    int status, sockfd, newfd; 
+    struct addrinfo hints, *res;
+    struct sockaddr_storage their_addr; 
+    socklen_t addr_size;  
+
+    // set up hints addrinfo 
     memset(&hints, 0, sizeof(hints)); 
     hints.ai_family = AF_UNSPEC; 
-    hints.ai_flags = AF_PASSIVE; 
     hints.ai_socktype = SOCK_STREAM; 
+    hints.ai_flags = AI_PASSIVE; 
 
-    if((status = getaddrinfo(,,&hints,&res)) != 0) {
+    // prepare the sockaddr through getaddrinfo 
+    if((status = getaddrinfo(NULL, MYPORT,&hints,&res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status)); 
         return 1; 
     }
 
-    for(struct addrinfo* p = res; p != NULL; p = p->ai_next) {
-        void* addr; 
-        char* ipver; 
-        if(p->ai_family == AF_INET) {
-            struct sockaddr_in *ipv4 = (struct sockaddr_in*)p->ai_addr;
-            addr = &(ipv4->sin_family); 
-            ipver = "IPv4"; 
-        }
-        else {
-            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6*)p->ai_addr;
-            addr = &(ipv6->sin6_family); 
-            ipver = "IPv6"; 
-        }
+    // create a socket, bind it to a port, and have it listen 
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol); 
+    bind(sockfd, res->ai_addr, res->ai_addrlen); 
+    listen(sockfd, BACKLOG); 
 
-        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr)); 
-    }
 
-    s = socket(res->ai_family, res->ai_socktype, res->ai_protocol); 
-
-    
+    freeaddrinfo(res); 
 
     return 0; 
 }
--- showip.c
+++ showip.c
@@ -9,7 +9,7 @@
 #include <netinet/in.h> 
 
 int main(int argc, char* argv[]) {
-    int status;
+    int status, sockfd;
     char ipstr[INET6_ADDRSTRLEN]; 
     struct addrinfo hints, *res; 
 
@@ -20,33 +20,35 @@ int main(int argc, char* argv[]) {
 
     memset(&hints, 0, sizeof(hints));   
     hints.ai_family = AF_UNSPEC; 
-    hints.ai_flags = AI_PASSIVE; 
     hints.ai_socktype = SOCK_STREAM; 
+    hints.ai_flags = AI_PASSIVE; 
 
-    if((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+    if((status = getaddrinfo(argv[1], "4500", &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status)); 
         return 2; 
     }
-
-    printf("IP addresses for %s:\n\n", argv[1]);
 
     for(struct addrinfo* p = res; p != NULL; p = p->ai_next) {
         char *ipver; 
         void *addr; 
         if(p->ai_family == AF_INET) {
             struct sockaddr_in *ipv4 = (struct sockaddr_in*)p->ai_addr; 
-            addr = &(ipv4->sin_family); 
+            addr = &(ipv4->sin_addr); 
             ipver = "IPv4"; 
         }
         else {
             struct sockaddr_in6 *ipv6 = (struct sockaddr_in6*)p->ai_addr; 
-            addr = &(ipv6->sin6_family); 
+            addr = &(ipv6->sin6_addr); 
             ipver = "IPv6";
         }
 
         inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr)); 
         printf("    %s: %s\n", ipver, ipstr); 
     }
+
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol); 
+
+    connect(sockfd, res->ai_addr, res->ai_addrlen); 
 
     freeaddrinfo(res); 
 
